3ddd61cf382406ada8268bf0958cb2a4c894d11b
a76u8 <you@example.com>
2022-06-08 15:37:57 +0300

    14

--- src/library/Library.java
+++ src/library/Library.java
@@ -1,13 +1,27 @@
 package library;
 
+import java.util.Arrays;
+
 public class Library {
     private Book[] books;
 
     public Library(Book[] books) {
-
+        for (int i = 0; i < books.length; i++) {
+            books[i] = ;
+        }
     }
 
-    public void getBook(Book book) {
-
+    public Book getBook(Book book) {
+        boolean flag = false;
+        for (int i = 0; i < books.length; i++) {
+            if (books[i].equals(book)) {
+                flag = true;
+            }
+        }
+        if (flag) {
+            return book;
+        } else {
+            return null;
+        }
     }
 }
--- src/library/Reader.java
+++ src/library/Reader.java
@@ -11,10 +11,16 @@ public class Reader {
     }
 
     public void takeBook(Library library, Book book) {
-
+        this.books.add(library.getBook(null));
     }
 
     public void returnBook(Library library, Book book) {
+        for (int i = 0; i < books.size(); i++) {
+            if (this.books.get(i).equals(book)) {
+                this.books.remove(i);
+                break;
+            }
+        }
 
     }
 }
