098fcdc4ab0769817999eb900e83603b0e02716c
danil <blnvdanil@gmail.com>
2022-06-29 19:17:33 +0300

    Files

---
+++ src/files/Files.java
@@ -0,0 +1,23 @@
+package files;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Files {
+
+
+    public static void main(String[] args) throws IOException {
+        FileReader reader = new FileReader("input.txt");
+
+        Scanner scan = new Scanner(reader);
+
+        System.out.println(scan.nextLine());
+
+        reader.close();
+    }
+}
+
+// char -- 'a'
+// String -- "hello"
+
