7b2c19dd67d1d05948e73ce719052cca0c48e517
a76u8 <you@example.com>
2022-07-27 18:55:54 +0300

    tabulation issues fixed

--- src/parsing/Tokenizer.java
+++ src/parsing/Tokenizer.java
@@ -18,56 +18,56 @@ public class Tokenizer {
     // есть ли следующий токен
     public boolean hasNext() {
 
-	if (currentToken != Token.NONE) {
-	    return true;
-	}
+	    if (currentToken != Token.NONE) {
+	        return true;
+	    }
 	
-	skipWhitespaces();
+	    skipWhitespaces();
 
         if (index == source.length()) {
-	    currentToken = Token.NONE;
+	        currentToken = Token.NONE;
             return false;
         }
 
         char ch = source.charAt(index);
 
-	if (ch == '(') {
-	    currentToken = Token.LEFT_PR;
-	    index++;
-	    return true;
-	}
+	    if (ch == '(') {
+	        currentToken = Token.LEFT_PR;
+	        index++;
+	        return true;
+	    }
 
-	if (ch == ')') {
-	    currentToken = Token.RIGHT_PR;
-	    index++;
-   	    return true;
-	}
+        if (ch == ')') {
+            currentToken = Token.RIGHT_PR;
+            index++;
+            return true;
+        }
 
-	if (ch == '+') {
-	    currentToken = Token.PLUS;
-	    index++;
-	    return true;
-	}
+        if (ch == '+') {
+            currentToken = Token.PLUS;
+            index++;
+            return true;
+        }
 
-	if (ch == '-') {
-	    currentToken = Token.MINUS;
-	    index++;
-	    return true;
-	}
+        if (ch == '-') {
+            currentToken = Token.MINUS;
+            index++;
+            return true;
+        }
 
-	if (ch == '/') {
-	    currentToken = Token.DIV;
-	    index++;
-	    return true;
-	}
+        if (ch == '/') {
+            currentToken = Token.DIV;
+            index++;
+            return true;
+        }
 
-	if (ch == '*') {
-	    currentToken = Token.MUL;
-	    index++;
-	    return true;
-	}
+        if (ch == '*') {
+            currentToken = Token.MUL;
+            index++;
+            return true;
+        }
 	
-	String num = "";
+	    String num = "";
 
         while (index != source.length() && '0' <= source.charAt(index) && source.charAt(index) <= '9') {
             num += source.charAt(index);
@@ -75,23 +75,22 @@ public class Tokenizer {
         }
 
         if (!num.equals("")) {
-	    currentTokenValue = num;
-	    currentToken = Token.NUMBER;
+	        currentTokenValue = num;
+	        currentToken = Token.NUMBER;
             return true;
         }
-	
-	currentToken = Token.NONE;
 
+        currentToken = Token.NONE;
         return false;
     }
 
     // следующий токен
     public Token nextToken() {
-	hasNext();
-	Token token = currentToken;
-	currentToken = Token.NONE;
+	    hasNext();
+	    Token token = currentToken;
+	    currentToken = Token.NONE;
 
-	return token;
+	    return token;
     }
 
     private void skipWhitespaces() {
@@ -103,15 +102,14 @@ public class Tokenizer {
     public static void main(String[] args) {
         String raw = "1 + 2 + 3 () + 2 - 1";
         Tokenizer tokenizer = new Tokenizer(raw);
+
         while (tokenizer.hasNext()) {
-	    Token token = tokenizer.nextToken();
-	    if (token == Token.NUMBER) {
-		System.out.print(token + "(" + tokenizer.getTokenValue() + ") ");
-	    } else {
-		System.out.print(token + " ");
-	    }
+	        Token token = tokenizer.nextToken();
+	        if (token == Token.NUMBER) {
+		        System.out.print(token + "(" + tokenizer.getTokenValue() + ") ");
+	        } else {
+		        System.out.print(token + " ");
+	        }
         }
     }
-
-
 }
